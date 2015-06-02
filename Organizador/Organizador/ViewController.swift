231cc9463469ba30a7b63f74ab5062cc890ff6a5
Vivian Chiodo Dias <vichiodo@MacBook-Pro-de-Vivian.local>
2015-06-02 14:09:48 -0300

    Projeto criado...

---
+++ Organizador/Organizador/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Organizador
+//
+//  Created by Vivian Chiodo Dias on 02/06/15.
+//  Copyright (c) 2015 Vivian Chiodo Dias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
